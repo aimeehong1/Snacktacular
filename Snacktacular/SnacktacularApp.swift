132b277b2f0bf4c4fb7190021b5af5919c3b5e09
Aimee Hong <honglp@bc.edu>
2024-11-01 00:26:47 -0400

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by Aimee Hong on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
